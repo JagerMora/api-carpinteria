c10805c5204b01686e837e9a4b72a5eb3cf1e024
JagerMora <78835594+MissaelMoraCar-E@users.noreply.github.com>
2023-11-28 03:35:40 -0600

    adding config.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
 const express = require('express')
 const furnituresRouter = require('./v1/routes/furnitures.js')
+const { PORT } = require('./config.js')
 
 const app = express()
 app.use(express.json())
-const PORT = process.env.PORT || 3000
 
 app.use('/api/v1', furnituresRouter)
 
